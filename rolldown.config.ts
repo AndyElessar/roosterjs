73128c2f2f00b1e49c1ae305f42dc8d6c2afc5c6
Andy Elesser <andy090517@gmail.com>
2025-10-24 15:05:41 +0800

    chore: update rolldown dependency to latest and add tsconfig to rolldown config

--- rolldown.config.ts
+++ rolldown.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'rolldown';
 export default defineConfig([
     {
         input: 'packages/roosterjs/lib/index.ts',
+        tsconfig: './packages/tsconfig.json',
         output: {
             file: 'dist/roosterjs.js',
             format: 'esm',
@@ -14,6 +15,7 @@ export default defineConfig([
     },
     {
         input: 'packages/roosterjs/lib/index.ts',
+        tsconfig: './packages/tsconfig.json',
         output: {
             file: 'dist/roosterjs.min.js',
             format: 'esm',
